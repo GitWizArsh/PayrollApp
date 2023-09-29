11050eb453072e881bb54755cc4fd2ec6a76e021
Arsh Singh <146023109+GitWizArsh@users.noreply.github.com>
2023-09-28 20:58:03 -0400

    Add files via upload

---
+++ Payroll-vertime/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Payroll_vertime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtPay = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblG_pay = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hours worked:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 119);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hourly pay rate:";
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(153, 41);
+            this.txtHours.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(76, 20);
+            this.txtHours.TabIndex = 3;
+            // 
+            // txtPay
+            // 
+            this.txtPay.Location = new System.Drawing.Point(153, 115);
+            this.txtPay.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPay.Name = "txtPay";
+            this.txtPay.Size = new System.Drawing.Size(76, 20);
+            this.txtPay.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 176);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Gross Pay:";
+            // 
+            // lblG_pay
+            // 
+            this.lblG_pay.AutoSize = true;
+            this.lblG_pay.Location = new System.Drawing.Point(142, 176);
+            this.lblG_pay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblG_pay.Name = "lblG_pay";
+            this.lblG_pay.Size = new System.Drawing.Size(0, 15);
+            this.lblG_pay.TabIndex = 7;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(44, 238);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(64, 25);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(130, 238);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(64, 25);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(216, 238);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(59, 25);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 289);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblG_pay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPay);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtPay;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblG_pay;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Payroll-vertime/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_vertime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                const decimal BASE_HOURS = 40m;
+                const decimal OT_MULTIPLIER = 1.5m;
+
+                decimal hoursWorked;
+                decimal hourlyPayRate;
+                decimal basePay;
+                decimal overtimeHours;
+                decimal overtimePay;
+                decimal grossPay;
+
+                hoursWorked = decimal.Parse(txtHours.Text);
+                hourlyPayRate = decimal.Parse(txtHours.Text);
+                
+
+                if(hoursWorked>BASE_HOURS)
+                {
+                    basePay = hourlyPayRate * BASE_HOURS;
+
+                    overtimeHours = hoursWorked - BASE_HOURS;
+
+                    overtimePay = overtimeHours * hourlyPayRate * OT_MULTIPLIER;
+
+                    grossPay = basePay + overtimePay;
+
+                }
+                else
+                {
+                    grossPay = hourlyPayRate * hoursWorked;
+                }
+                lblG_pay.Text = grossPay.ToString();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtHours.Text = "";
+            txtPay.Text = "";
+          
+
+            lblG_pay.Text = "";
+
+            txtHours.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+}
